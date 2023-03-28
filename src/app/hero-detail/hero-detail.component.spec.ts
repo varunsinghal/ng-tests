fd30f188e978f60170554cbfc0d2ca4a15863ca3
varunsinghal <varunsinghal15@gmail.com>
2023-03-28 12:15:15 +0530

    unit testing activated route #6

---
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,43 @@
+import { Location } from "@angular/common";
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { FormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
+import { HeroService } from "../hero.service";
+import { HeroDetailComponent } from "./hero-detail.component"
+
+describe("HeroDetailComponent", () => {
+    let fixture: ComponentFixture<HeroDetailComponent>;
+    let mockHeroService: jasmine.SpyObj<HeroService>;
+    let mocklocation: jasmine.SpyObj<Location>;
+    let mockActivatedRoute = {
+        snapshot: {
+            paramMap: {
+                get: () => { return '2' }
+            }
+        }
+    }
+
+    beforeEach(() => {
+        mockHeroService = jasmine.createSpyObj(HeroService, ['getHero', 'updateHero']);
+        mocklocation = jasmine.createSpyObj(Location, ['back']);
+        TestBed.configureTestingModule({
+            imports: [FormsModule],
+            declarations: [HeroDetailComponent],
+            providers: [
+                {provide: ActivatedRoute, useValue: mockActivatedRoute},
+                {provide: HeroService, useValue: mockHeroService},
+                {provide: Location, useValue: mocklocation},
+            ]
+        });
+        fixture = TestBed.createComponent(HeroDetailComponent);
+    })
+
+    it("should load correct hero detail when initialized", () => {
+        mockHeroService.getHero.and.returnValue(of({id: 2, name: 'hero-name', strength: 3}));
+        fixture.detectChanges();
+        expect(fixture.debugElement.query(By.css('h2')).nativeElement.textContent).toContain('HERO-NAME');
+        expect(mockHeroService.getHero).toHaveBeenCalledWith(2);
+    })
+})
